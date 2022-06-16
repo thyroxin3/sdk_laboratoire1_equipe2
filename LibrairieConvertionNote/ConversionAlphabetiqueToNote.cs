e5513a01b91e392f065f180cf177f93a4cda4ea6
Alex Nellis <2215024@grasset.qc.ca>
2022-06-15 21:06:58 -0400

    passapr/liminaire

---
+++ LibrairieConvertionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,60 @@
+﻿namespace LibrairieConvertionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public void Transformer()
+        {
+        bool error =true;
+            do{
+                string noteMusicale = '01';
+                Console.WriteLine ("entrez une note musicale entre A et G");
+                Console.ReadLine(""+noteMusicale);
+                if (noteMusicale = 'A') 
+                {
+                    Console.WriteLine("La");
+                    error = false;
+                }
+                if (noteMusicale = 'B') 
+                {
+                    Console.WriteLine("Si");
+                    error = false;
+                }
+                if (noteMusicale = 'C') 
+                {
+                    Console.WriteLine("Do");
+                    error = false;
+                }
+                if (noteMusicale = 'D') 
+                {
+                    Console.WriteLine("Re");
+                    error = false;
+                }
+
+                if (noteMusicale = 'E') 
+                {
+                    Console.WriteLine("Mi");
+                    error = false;
+                }
+
+                if (noteMusicale = 'F') 
+                {
+                    Console.WriteLine("Fa");
+                    error = false;
+                }
+
+                if (noteMusicale = 'G') 
+                {
+                    Console.WriteLine("Sol");
+                    error = false;
+                }
+
+                else 
+                {
+                    Console.WriteLine("entree invalide");
+                }
+             
+            }while (error=true) 
+        }
+
+    }
+}
--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LibrairieConvertionNote;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
