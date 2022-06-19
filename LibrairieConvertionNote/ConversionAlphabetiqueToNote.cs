b47c2a14f8f977b7aac6c0619d59438715d9bb32
thyroxin3 <thyroxine@rocketmail.com>
2022-06-19 15:02:29 -0400

    Version_dimanche

--- LibrairieConvertionNote/ConversionAlphabetiqueToNote.cs
+++ LibrairieConvertionNote/ConversionAlphabetiqueToNote.cs
@@ -2,59 +2,19 @@
 {
     public class ConversionAlphabetiqueToNote
     {
-        public void Transformer()
+        static void TableDeHachage()
         {
-        bool error =true;
-            do{
-                string noteMusicale = '01';
-                Console.WriteLine ("entrez une note musicale entre A et G");
-                Console.ReadLine(""+noteMusicale);
-                if (noteMusicale = 'A') 
-                {
-                    Console.WriteLine("La");
-                    error = false;
-                }
-                if (noteMusicale = 'B') 
-                {
-                    Console.WriteLine("Si");
-                    error = false;
-                }
-                if (noteMusicale = 'C') 
-                {
-                    Console.WriteLine("Do");
-                    error = false;
-                }
-                if (noteMusicale = 'D') 
-                {
-                    Console.WriteLine("Re");
-                    error = false;
-                }
-
-                if (noteMusicale = 'E') 
-                {
-                    Console.WriteLine("Mi");
-                    error = false;
-                }
-
-                if (noteMusicale = 'F') 
-                {
-                    Console.WriteLine("Fa");
-                    error = false;
-                }
-
-                if (noteMusicale = 'G') 
-                {
-                    Console.WriteLine("Sol");
-                    error = false;
-                }
-
-                else 
-                {
-                    Console.WriteLine("entree invalide");
-                }
-             
-            }while (error=true) 
+            Console.WriteLine("----TableDeHachage()-----\n");
+            Hashtable hs = new Hashtable(); // Création d'une table de hachage
+            hs.Add("A", "LA"); // La table de hachage est aussi une combinaison de Clef/Valeur, mais non typé
+            hs.Add("B", "SI");
+            hs.Add("C", "DO");
+            hs.Add("D", "RE");
+            hs.Add("E", "MI");
+            hs.Add("F", "FA");
+            hs.Add("G", "SOL");
         }
 
     }
+
 }
--- SDK_Laboratoire1/Program.cs
+++ SDK_Laboratoire1/Program.cs
@@ -1,4 +1,50 @@
 ﻿using LibrairieConvertionNote;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+   while (true)
+    {
+         string noteMusicale = "";
+         Console.WriteLine ("Entrez une note musicale entre A et G");
+         noteMusicale = Console.ReadLine();
+         
+        if (noteMusicale == 'A') 
+            {
+                Console.WriteLine("LA");
+  
+            }
+
+         if (noteMusicale == 'B') 
+            {
+                Console.WriteLine("SI");
+            }
+
+         if (noteMusicale == 'C') 
+            {
+                Console.WriteLine("DO");
+            }
+
+         if (noteMusicale == 'D') 
+            {
+                Console.WriteLine("RE");
+            }
+
+         if (noteMusicale == 'E') 
+            {
+                Console.WriteLine("MI");
+            }
+
+         if (noteMusicale == 'F') 
+            {
+                Console.WriteLine("FA");
+            }
+
+         if (noteMusicale == 'G') 
+            {
+                Console.WriteLine("SOL");
+            }
+
+         else 
+            {
+                Console.WriteLine("Entree invalide");
+            }
+             
+      }
